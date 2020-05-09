f338219f4e035076e144f7d4938da08078bed89b
Matt Gordon <bigmattsize21@gmail.com>
2020-05-09 03:45:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,7 +56,7 @@ class Student
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
     student = DB[:conn].execute(sql, name)[0]
-    self.new_from_db(student)
+    self.new_from_db(student,name)
   end
   
   def update
