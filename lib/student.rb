0207fb3595ebaa7aa39f86d15cf8a32fac4cc818
Matt Gordon <bigmattsize21@gmail.com>
2020-05-09 03:41:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,7 +54,10 @@ class Student
   end
   
   def self.find_by_name(name)
-    
+    sql = "SELECT * FROM songs WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Song.new(result[0], result[1], result[2])
+  end
   
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
