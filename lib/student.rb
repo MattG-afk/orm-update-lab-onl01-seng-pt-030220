da8bf7663e57e7cf5bb4b07f7903cf5b93bc46b5
Matt Gordon <bigmattsize21@gmail.com>
2020-05-09 03:29:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,6 +62,9 @@ class Student
     end.first
   end
   
-  
+  def update
+    sql = "UPDATE songs SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
 end
