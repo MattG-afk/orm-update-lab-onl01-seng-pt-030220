dd7567982f8017b3dced1018116c33f6bd1b4f71
Matt Gordon <bigmattsize21@gmail.com>
2020-05-09 02:42:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,9 @@ class Student
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXSITS students (
-    id INTEGER PRIMARY KEY)
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    grade INTEGER)
 
 
 end
